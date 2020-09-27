2db5c63b8dd0f926477ea6ab2824a9d7aaccfe79
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-09-27 20:47:03 +0530

    Power of two

---
+++ ForLoopPrograms/PowerOfTwo.sh
@@ -0,0 +1,16 @@
+#! /bin/bash  -x
+
+#Enter exponent
+echo "Enter exponent:"
+read exp
+
+#declare variables
+base=2
+num=1
+
+#Using for loop calculate power of 2
+for((i=1;i<=$exp;i++))
+do
+	num=$(( $num * $base ))
+	echo $num
+done
